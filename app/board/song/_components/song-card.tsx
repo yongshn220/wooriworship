ba9e0d57f288c829551eadd21675c1c0d7311403
Yongjung Shin <yongshn220@gmail.com>
2024-03-25 20:28:48 -0400

    update song card design

--- app/board/song/_components/song-card.tsx
+++ app/board/song/_components/song-card.tsx
@@ -2,27 +2,35 @@ import {HoverOverlay} from "@/components/hover-overlay";
 import Link from "next/link";
 import LinkIcon from '@/public/icons/linkIcon.svg'
 import HeartIcon from '@/public/icons/heartIcon.svg'
+import {Badge} from "@/components/ui/badge";
 
-const tags = ["빠른", "신나는", "엔딩곡", "강한비트", "경쾌한", "신나는", "엔딩곡", "강한비트", "경쾌한"]
+const tags = ["빠른", "신나는", "엔딩곡", "강한비트", "경쾌한", "신나는"]
 
 export function SongCard() {
   return (
     <div>
-      <div
-        className="group aspect-[1/1] border rounded-lg flex flex-col overflow-hidden bg-[#95ABCC] cursor-pointer">
-        <div className="relative h-full flex-center flex-col text-white">
+      <div className="aspect-[1/1] border rounded-lg flex flex-col overflow-hidden bg-[#95ABCC]">
+        <div className="relative group h-full flex-center flex-col text-white cursor-pointer">
           <HoverOverlay/>
           <p className="font-semibold text-lg">내 주를 가까이</p>
           <p className="text-sm">Isaiah6tyone</p>
         </div>
         <div className="flex-between bg-white p-4">
-          <HeartIcon/>
-          <LinkIcon/>
+          <div className="cursor-pointer">
+            <HeartIcon/>
+          </div>
+          <div className="cursor-pointer">
+            <LinkIcon/>
+          </div>
         </div>
       </div>
-      <p className="w-full text-center text-sm text-gray-600 mt-1">
-        March 30 <span className="text-xs">(Mon)</span>
-      </p>
+      <div className="w-full text-left text-sm mt-1 space-x-2 space-y-2">
+        {
+          tags.slice(0,Math.floor(Math.random() * 10 + 1)).map((tag,i) => (
+            <Badge key={i} variant="outline">{tag}</Badge>
+          ))
+        }
+      </div>
     </div>
   )
 }
