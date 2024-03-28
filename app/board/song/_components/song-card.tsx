ef4a806329eba08ddeb900f2466741a0d0ac5a1c
Yongjung Shin <yongshn220@gmail.com>
2024-03-28 19:09:19 -0400

    Add | song detail card structure

--- app/board/song/_components/song-card.tsx
+++ app/board/song/_components/song-card.tsx
@@ -1,16 +1,21 @@
+'use client'
 import {HoverOverlay} from "@/components/hover-overlay";
-import Link from "next/link";
 import LinkIcon from '@/public/icons/linkIcon.svg'
 import HeartIcon from '@/public/icons/heartIcon.svg'
 import {Badge} from "@/components/ui/badge";
+import {SongDetailCard} from "@/app/board/song/_components/song-detail-card";
+import {useState} from "react";
 
 const tags = ["빠른", "신나는", "엔딩곡", "강한비트", "경쾌한", "신나는"]
 
 export function SongCard() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div>
+    <div className="h-full">
+      <SongDetailCard isOpen={isOpen} setIsOpen={setIsOpen}/>
       <div className="aspect-[1/1] border rounded-lg flex flex-col overflow-hidden bg-[#95ABCC]">
-        <div className="relative group h-full flex-center flex-col text-white cursor-pointer">
+        <div className="relative group h-full flex-center flex-col text-white cursor-pointer" onClick={() => setIsOpen(true)}>
           <HoverOverlay/>
           <p className="font-semibold text-lg">내 주를 가까이</p>
           <p className="text-sm">Isaiah6tyone</p>
@@ -26,7 +31,7 @@ export function SongCard() {
       </div>
       <div className="w-full text-left text-sm mt-1 space-x-2 space-y-2">
         {
-          tags.slice(0,Math.floor(Math.random() * 10 + 1)).map((tag,i) => (
+          tags.map((tag,i) => (
             <Badge key={i} variant="outline">{tag}</Badge>
           ))
         }
---
+++ app/board/song/_components/song-detail-card.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import {Plus} from "lucide-react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription, DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {TeamIcon} from "@/components/team-icon";
+import {Button} from "@/components/ui/button";
+import {Textarea} from "@/components/ui/textarea";
+import {useState} from "react";
+import {DatePicker} from "@/app/board/_components/worship-plan/date-picker";
+import {NewSongCard} from "@/app/board/_components/worship-plan/new-song-card";
+import {SongCard} from "@/app/board/song/_components/song-card";
+
+interface Props {
+  isOpen: boolean
+  setIsOpen: any
+}
+
+export function SongDetailCard({isOpen, setIsOpen}: Props) {
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="sm:max-w-[600px] h-5/6 overflow-y-scroll scrollbar-hide">
+        <DialogHeader>
+          <DialogTitle className="text-2xl">Add New Song</DialogTitle>
+          <DialogDescription>Create and add new song in the song board</DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-6 py-4">
+          <div className="flex-center gap-2">
+            <TeamIcon name="GVC Friday"/>
+            <p className="font-bold text-sm">GVC Friday</p>
+          </div>
+          <div className="flex-start flex-col items-center gap-1.5">
+            <Label htmlFor="name" className="text-">
+              Title
+            </Label>
+            <p>Isaiash6tyOne</p>
+          </div>
+          <div className="flex-start flex-col items-center gap-1.5">
+            <Label htmlFor="name" className="text-">
+              Link
+            </Label>
+            <Input
+              id="name"
+              placeholder="Related link for the song"
+            />
+          </div>
+          <div className="flex-start flex-col items-center gap-1.5">
+            <Label htmlFor="name" className="text-">
+              Version
+            </Label>
+            <Input
+              id="name"
+              placeholder="Version of the song"
+            />
+          </div>
+          <div className="flex-start flex-col items-center gap-1.5">
+            <Label htmlFor="name" className="text-">
+              Music Sheets
+            </Label>
+            <div className="flex-start w-full h-60 aspect-square border-2 p-2 rounded-md shadow-sm">
+              <div className="h-full flex-center flex-col p-6">
+                <Plus className="h-[50px] w-[50px] rounded-full p-2 text-white bg-blue-500 hover:bg-blue-400 cursor-pointer"/>
+              </div>
+              <div className="flex w-full h-full gap-4 overflow-x-scroll scrollbar-hide">
+                <div className="flex flex-col h-full aspect-[3/4]">
+                  <div className="flex-1 bg-gray-100"/>
+                  <p className="text-center text-sm text-gray-500">1</p>
+                </div>
+
+                <div className="flex flex-col h-full aspect-[3/4]">
+                  <div className="flex-1 bg-gray-100"/>
+                  <p className="text-center text-sm text-gray-500">2</p>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex-center">
+        </div>
+        <DialogFooter>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
