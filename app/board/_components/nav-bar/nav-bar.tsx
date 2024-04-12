83a5813d863872a5a0f06e5f8f9fb693b9b58a31
Yongjung Shin <yongshn220@gmail.com>
2024-04-12 19:24:46 -0400

    fix

--- app/board/_components/nav-bar/nav-bar.tsx
+++ app/board/_components/nav-bar/nav-bar.tsx
@@ -5,7 +5,7 @@ import {ManageTeamButton} from "@/app/board/_components/nav-bar/manage-team-butt
 import {SearchInput} from "@/app/board/_components/nav-bar/search-input";
 import {SearchTags} from "@/app/board/_components/nav-bar/search-tags";
 import {cn} from "@/lib/utils";
-import {Routes} from "@/components/constants/enums";
+import {Page, Routes} from "@/components/constants/enums";
 import {useRecoilValue} from "recoil";
 import {currentPageAtom} from "@/app/board/_states/pageState";
 import {ProfileButton} from "@/app/board/_components/nav-bar/profile-button";
@@ -15,7 +15,7 @@ export function Navbar() {
   return (
     <div className="flex-between p-5 gap-4">
       <div className="flex-1">
-        <div className={cn("hidden w-full max-w-xl gap-4", {"flex-center": currentPage === Routes.SONG})}>
+        <div className={cn("hidden w-full max-w-xl gap-4", {"flex-center": currentPage === Page.SONG})}>
           <SearchInput/>
           <SearchTags/>
         </div>
