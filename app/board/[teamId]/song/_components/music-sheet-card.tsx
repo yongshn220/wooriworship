b898dd5a6dcbd0702053744e23c99f0fe2e51a0d
Yongjung Shin <yongshn220@gmail.com>
2024-04-26 15:05:09 -0400

    Update Song Edit

--- app/board/[teamId]/song/_components/music-sheet-card.tsx
+++ app/board/[teamId]/song/_components/music-sheet-card.tsx
@@ -1,27 +1,32 @@
-import {MusicSheet} from "@/app/board/[teamId]/song/_components/new-button";
 import Image from "next/image"
 import {LoadingCircle} from "@/components/animation/loading-indicator";
+import {MusicSheet} from "@/app/board/[teamId]/song/_components/song-form";
+import {Cross2Icon} from "@radix-ui/react-icons";
 
 interface Props {
   musicSheet: MusicSheet
   index: number
+  handleRemoveImage: Function
 }
 
-export function MusicSheetCard({musicSheet, index}: Props) {
+export function MusicSheetCard({musicSheet, index, handleRemoveImage}: Props) {
   return (
     <div className="relative flex flex-col h-full aspect-[3/4]">
-      <div className="relative flex-1 flex-center bg-gray-100 rounded-md">
+      <div className="relative flex-1 flex-start bg-gray-100 rounded-md">
         {
           musicSheet.isLoading ?
           <LoadingCircle/>
           :
-          <Image
-            src={musicSheet.url}
-            fill={true}
-            sizes="20vw"
-            alt="store post image"
-            className="object-fit rounded-md p-1"
-          />
+          <>
+            <Image
+              src={musicSheet.url}
+              fill
+              sizes="20vw, 20vw, 20vw"
+              className="object-contain p-1 rounded-md"
+              alt="Music sheet image"
+            />
+            <Cross2Icon className="absolute right-1 top-1 p-0.5 cursor-pointer bg-gray-600 hover:bg-black rounded-full text-white" width={17} height={17} onClick={() => handleRemoveImage(index)}/>
+          </>
         }
       </div>
       <p className="text-center text-xs text-gray-400 mt-1 font-semibold">{index + 1}</p>
