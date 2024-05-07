cb6f9ee323c5b825a1d96c0f394ed8234f0ada77
Yongjung Shin <yongshn220@gmail.com>
2024-05-06 21:04:52 -0400

    update | worship sidebar

--- components/sidebar/md-sidebar.tsx
+++ components/sidebar/md-sidebar.tsx
@@ -1,8 +1,11 @@
+import {cn} from "@/lib/utils";
 
-export function MdSidebar({ children }: Readonly<{ children: React.ReactNode }>) {
+export function MdSidebar({ children, className }: any) {
   return (
-    <div className="hidden sm:block w-[170px] h-full lg:w-[206px]">
-      <div className="fixed flex flex-col space-y-6 w-[170px] h-full lg:w-[206px] pl-5 pt-5">{children}</div>
+    <div className="hidden sm:block w-[170px] h-full lg:w-[206px] ">
+      <div className={cn("fixed flex flex-col space-y-6 w-[170px] h-full lg:w-[206px] pl-5 pt-5", className)}>
+        {children}
+      </div>
     </div>
   )
 }
