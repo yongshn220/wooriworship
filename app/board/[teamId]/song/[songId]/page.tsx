252a56b1dc44923d924f9623ff11ed8d67be3561
Yongjung Shin <yongshn220@gmail.com>
2024-05-13 20:27:57 -0400

    minor update

--- app/board/[teamId]/song/[songId]/page.tsx
+++ app/board/[teamId]/song/[songId]/page.tsx
@@ -7,17 +7,25 @@ import {toPlainObject} from "@/components/helper/helper-functions";
 import {useEffect, useState} from "react";
 import {Song} from "@/models/song";
 import {getPathSong} from "@/components/helper/routes";
+import {DataFetchStatus} from "@/components/constants/enums";
+import {toast} from "@/components/ui/use-toast";
 
 
 export default function SongDetailPage({params}: any) {
   const teamId = params.teamId
   const songId = params.songId
-  const [song, setSong] = useState<Song>(null)
+  const [songWrapper, setSongWrapper] = useState<{ song: Song, status: DataFetchStatus }>({
+    song: null,
+    status: DataFetchStatus.PROCESSING
+  })
   const router = useRouter()
 
   useEffect(() => {
     SongService.getById(songId).then(_song => {
-      setSong(_song as Song)
+      setSongWrapper({
+        song: _song as Song,
+        status: (_song)? DataFetchStatus.VALID : DataFetchStatus.INVALID
+      })
     })
   }, [songId])
 
@@ -27,9 +35,19 @@ export default function SongDetailPage({params}: any) {
     }
   }
 
-  if (!song) return <></>
+  if (songWrapper.status === DataFetchStatus.PROCESSING) {
+    return <></>
+  }
+  if (songWrapper.status === DataFetchStatus.INVALID) {
+    toast({
+      title: `Invalid song`,
+      description: "Selected song is deleted or not exist."
+    })
+    router.push(getPathSong(teamId))
+    return <></>
+  }
 
   return (
-     <SongDetailCard isOpen={true} setIsOpen={onOpenChangeHandler} song={toPlainObject(song)} editable={true} />
+     <SongDetailCard isOpen={true} setIsOpen={onOpenChangeHandler} song={toPlainObject(songWrapper.song)} editable={true} />
   )
 }
