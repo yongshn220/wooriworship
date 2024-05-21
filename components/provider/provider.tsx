357ee853efb51e5c68989fe88a5a9e5593c1a762
Yongjung Shin <yongshn220@gmail.com>
2024-05-20 23:40:52 -0400

    hot fix

--- components/provider/provider.tsx
+++ components/provider/provider.tsx
@@ -1,14 +1,11 @@
 "use client"
 import React from 'react';
 import {RecoilRoot} from "recoil";
-import {SessionProvider} from "next-auth/react";
 
 export function Provider({children}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <SessionProvider>
-      <RecoilRoot>
-        {children}
-      </RecoilRoot>
-    </SessionProvider>
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
   )
 }
