d54053acdf7dec252acd5e1d2cdc7d19b5faf916
Yongjung Shin <yongshn220@gmail.com>
2024-05-20 22:47:16 -0400

    empty build test

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,40 @@
 'use client'
-import {RoutingPage} from "@/app/_components/routing-page";
-import {LandingPage} from "@/app/_components/landing-page";
-import {useEffect, useState} from "react";
-import {auth} from "@/firebase";
-import {getPathBoard} from "@/components/helper/routes";
-import {useRouter} from "next/navigation";
-
-enum AuthStatus {
-  PROCESSING,
-  VALID,
-  NOT_VALID,
-}
+// import {RoutingPage} from "@/app/_components/routing-page";
+// import {LandingPage} from "@/app/_components/landing-page";
+// import {useEffect, useState} from "react";
+// import {auth} from "@/firebase";
+// import {getPathBoard} from "@/components/helper/routes";
+// import {useRouter} from "next/navigation";
+//
+// enum AuthStatus {
+//   PROCESSING,
+//   VALID,
+//   NOT_VALID,
+// }
 
 export default function Home() {
-  const [authStatus, setAuthStatus] = useState<AuthStatus>(AuthStatus.PROCESSING)
-  const router = useRouter()
-
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        router.replace(getPathBoard())
-      }
-      else {
-        setAuthStatus(AuthStatus.NOT_VALID)
-      }
-    });
-  }, [router]);
+  // const [authStatus, setAuthStatus] = useState<AuthStatus>(AuthStatus.PROCESSING)
+  // const router = useRouter()
+  //
+  // useEffect(() => {
+  //   auth.onAuthStateChanged((authUser) => {
+  //     if (authUser) {
+  //       router.replace(getPathBoard())
+  //     }
+  //     else {
+  //       setAuthStatus(AuthStatus.NOT_VALID)
+  //     }
+  //   });
+  // }, [router]);
 
   return (
     <div>
-      {
-        authStatus === AuthStatus.NOT_VALID
-          ? <LandingPage/>
-          : <RoutingPage/>
-      }
+      build test
+      {/*{*/}
+      {/*  authStatus === AuthStatus.NOT_VALID*/}
+      {/*    ? <LandingPage/>*/}
+      {/*    : <RoutingPage/>*/}
+      {/*}*/}
     </div>
   )
 }
