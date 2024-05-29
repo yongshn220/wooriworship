41b0b971f9afe9f2eb4a435d00df340e59c31e1d
Yongjung Shin <yongshn220@gmail.com>
2024-05-28 21:16:28 -0400

    Fix | screen locked after song edited

--- app/board/[teamId]/song/_components/menu-button.tsx
+++ app/board/[teamId]/song/_components/menu-button.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuSeparator, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
 import MenuIcon from "@/public/icons/menuIcon.svg";
 import {DeleteConfirmationDialog} from "@/components/dialog/delete-confirmation-dialog";
 import {useState} from "react";
@@ -11,6 +11,7 @@ import {SongService} from "@/apis";
 import {toast} from "@/components/ui/use-toast";
 import {CopyIcon, SquarePen, Trash2Icon, LinkIcon, DownloadIcon} from "lucide-react";
 import {currentTeamSongIdsAtom} from "@/global-states/song-state";
+import {Button} from "@/components/ui/button";
 
 interface Props {
   songTitle: string
@@ -20,10 +21,10 @@ interface Props {
 export function MenuButton({songTitle, songId}: Props) {
   const teamId = useRecoilValue(currentTeamIdAtom)
   const setCurrentTeamSongIds = useSetRecoilState(currentTeamSongIdsAtom)
-  const [isOpen, setIsOpen] = useState(false)
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false)
   const router = useRouter()
 
-  function handleEditSong() {
+  async function handleEditSong() {
     router.push(getPathSongEdit(teamId, songId))
   }
 
@@ -50,36 +51,36 @@ export function MenuButton({songTitle, songId}: Props) {
 
   return (
     <>
-      <DeleteConfirmationDialog isOpen={isOpen} setOpen={setIsOpen} title="Delete Song" description={`Do you really want to delete [${songTitle}]? This action can't be undone.`} onDeleteHandler={handleDeleteSong}/>
+      <DeleteConfirmationDialog isOpen={isDeleteDialogOpen} setOpen={setDeleteDialogOpen} title="Delete Song" description={`Do you really want to delete [${songTitle}]? This action can't be undone.`} onDeleteHandler={handleDeleteSong}/>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <MenuIcon/>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="min-w-[200px] p-2">
-          <DropdownMenuGroup className="space-y-2">
-            <DropdownMenuItem disabled className="cursor-pointer" onClick={() => handleEditSong()}>
+        <DropdownMenuContent className="min-w-[200px] p-2 flex-center flex-col">
+          <DropdownMenuGroup className="space-y-2 w-full">
+            <Button variant="ghost" disabled className="cursor-pointer w-full flex-start pl-2" onClick={() => handleEditSong()}>
                <DownloadIcon className="mr-3 w-5 h-5"/>
                <p>Download Score</p>
-             </DropdownMenuItem>
+             </Button>
           </DropdownMenuGroup>
           <DropdownMenuSeparator/>
-          <DropdownMenuGroup className="space-y-2">
-            <DropdownMenuItem disabled className="cursor-pointer">
+          <DropdownMenuGroup className="space-y-2 flex-center flex-col w-full">
+            <Button variant="ghost" disabled className="cursor-pointer w-full flex-start pl-2">
                <LinkIcon className="mr-3 w-5 h-5"/>
                <p>Copy Link</p>
-             </DropdownMenuItem>
-            <DropdownMenuItem disabled className="cursor-pointer">
+             </Button>
+            <Button variant="ghost" disabled className="cursor-pointer w-full flex-start pl-2">
                <CopyIcon className="mr-3 w-5 h-5"/>
                <p>Duplicate</p>
-             </DropdownMenuItem>
-             <DropdownMenuItem className="cursor-pointer" onClick={() => handleEditSong()}>
+             </Button>
+             <Button variant="ghost" className="cursor-pointer w-full flex-start pl-2" onClick={() => handleEditSong()}>
                <SquarePen className="mr-3 w-5 h-5"/>
                <p>Edit</p>
-             </DropdownMenuItem>
-             <DropdownMenuItem className="text-red-600 focus:bg-red-50 focus:text-red-500 cursor-pointer" onClick={() => setIsOpen((prev) => !prev)}>
+             </Button>
+             <Button variant="ghost" className="text-red-600 focus:bg-red-50 focus:text-red-500 cursor-pointer w-full flex-start pl-2" onClick={() => setDeleteDialogOpen((prev) => !prev)}>
                <Trash2Icon className="mr-3 w-5 h-5"/>
                <p>Delete</p>
-             </DropdownMenuItem>
+             </Button>
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
