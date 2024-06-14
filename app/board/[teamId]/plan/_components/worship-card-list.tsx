a5a11f3fb2f1adbefc61eb5b2108015a0113e945
Yongjung Shin <yongshn220@gmail.com>
2024-06-13 22:00:19 -0400

    Update | mobile view

--- app/board/[teamId]/plan/_components/worship-card-list.tsx
+++ app/board/[teamId]/plan/_components/worship-card-list.tsx
@@ -17,7 +17,7 @@ export function WorshipCardList() {
         <div className="w-full h-full">
           {
             (worshipIdsLoadable.contents?.length > 0) ?
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-10 mt-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-10 mt-10">
               {
                 worshipIdsLoadable.contents.map((worshipId: string) => (
                   <WorshipCard key={worshipId} worshipId={worshipId}/>
