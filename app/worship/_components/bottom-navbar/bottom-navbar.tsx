e843bd32c3ac289afe89a18418faef986d706ee7
Yongjung Shin <yongshn220@gmail.com>
2024-06-20 23:16:36 -0400

    Fix | worship edit local update

--- app/worship/_components/bottom-navbar/bottom-navbar.tsx
+++ app/worship/_components/bottom-navbar/bottom-navbar.tsx
@@ -24,7 +24,7 @@ export function BottomNavbar({teamId, worshipId}: Props) {
   const router = useRouter()
 
   return (
-    <div className="bottom-0 w-full h-[80px] bg-white shadow shadow-top">
+    <div className="lg:hidden bottom-0 w-full h-[80px] bg-white shadow shadow-top">
       <div className="w-full h-full flex justify-between px-5">
         <div className={cn("w-16 h-16 flex-center flex-col text-gray-500 cursor-pointer", {"text-black": (currentPage === Page.PLAN)})}
           onClick={() => router.push(getPathPlan(currentTeamId))}>
