b5f2e32d47ae5d7e4e0cf82406c4a1422bfe44ca
Yongjung Shin <yongshn220@gmail.com>
2024-06-25 15:12:12 -0400

    update song list uii

--- app/board/[teamId]/song/_components/song-list-item.tsx
+++ app/board/[teamId]/song/_components/song-list-item.tsx
@@ -23,9 +23,9 @@ export function SongListItem({songId}: Props) {
     <div className="flex w-full px-4 rounded-lg cursor-pointer py-2 my-2 hover:bg-gray-100" onClick={handleSongCardClick}>
       <div className="flex-1 flex flex-col">
         <p className="font-semibold text-md">{song?.title} {song?.key? `[${song.key}]` : ""}</p>
-        <p className="text-sm text-gray-500">{song?.original.author}</p>
+        <p className="text-sm text-gray-500">{song?.original.author === "" ? "unknown" : song?.original.author}</p>
       </div>
-      <div className="hidden lg:flex flex-[0.4] flex-end items-center text-gray-500 text-sm">
+      <div className="hidden lg:flex lg:flex-[0.4] flex-end items-center text-gray-500 text-sm">
         <div className="hidden lg:flex">{song?.version}</div>
       </div>
       <div className="hidden sm:flex flex-1 justify-end items-center w-full text-left text-sm gap-2">
@@ -35,7 +35,7 @@ export function SongListItem({songId}: Props) {
           ))
         }
       </div>
-      <div className="flex flex-1 lg:flex-[0.5] justify-end items-center text-gray-500 text-sm">
+      <div className="flex lg:flex-[0.5] justify-end items-center text-gray-500 text-sm pl-2">
         <div className="lg:hidden">{timestampToDatePassedFromNowShorten(song?.last_used_time)}</div>
         <div className="hidden lg:flex">{timestampToDatePassedFromNow(song?.last_used_time)}</div>
       </div>
