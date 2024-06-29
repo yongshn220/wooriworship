1da8c6cdc2da6c2c916fca608f132700a810903d
Yongjung Shin <yongshn220@gmail.com>
2024-06-29 00:08:55 -0400

    Code Cleansing

--- app/board/[teamId]/plan/_components/song-select-card-list.tsx
+++ app/board/[teamId]/plan/_components/song-select-card-list.tsx
@@ -1,41 +1,41 @@
-import {Song} from "@/models/song";
-import {SongSelectCard} from "@/app/board/[teamId]/plan/_components/song-select-card";
-import {useRecoilValue} from "recoil";
-import {selectedSongInfoListAtom} from "@/app/board/[teamId]/plan/_components/status";
-import {useMemo} from "react";
-import {useDebounce} from "use-debounce";
-
-interface Props {
-  teamId: string
-  searchInput: string
-  songList: Array<Song>
-}
-
-export function SongSelectCardList({teamId, searchInput, songList}: Props) {
-  const selectedSongInfoList = useRecoilValue(selectedSongInfoListAtom)
-  const [debounced] = useDebounce(searchInput, 500)
-
-  const preprocessedSongList = useMemo(() => {
-    const keyword = debounced.replace(/[a-zA-Z\uAC00-\uD7AF]/g, (match) => match.toLowerCase())
-    console.log(keyword)
-    return songList.filter((song) => song.title.includes(keyword))
-  }, [songList, debounced])
-
-  const selectedSongIds = useMemo(() => selectedSongInfoList.map((info) => info?.song?.id), [selectedSongInfoList])
-
-  return (
-    <div
-      className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-10 mt-10">
-      {
-        preprocessedSongList.map((song: Song) => (
-          <SongSelectCard
-            teamId={teamId}
-            key={song.id}
-            songId={song.id}
-            isSelected={selectedSongIds.includes(song.id)}
-          />
-        ))
-      }
-    </div>
-  )
-}
+// import {Song} from "@/models/song";
+// import {SongSelectCard} from "@/app/board/[teamId]/plan/_components/song-select-card";
+// import {useRecoilValue} from "recoil";
+// import {selectedSongInfoListAtom} from "@/app/board/[teamId]/plan/_components/status";
+// import {useMemo} from "react";
+// import {useDebounce} from "use-debounce";
+//
+// interface Props {
+//   teamId: string
+//   searchInput: string
+//   songList: Array<Song>
+// }
+//
+// export function SongSelectCardList({teamId, searchInput, songList}: Props) {
+//   const selectedSongInfoList = useRecoilValue(selectedSongInfoListAtom)
+//   const [debounced] = useDebounce(searchInput, 500)
+//
+//   const preprocessedSongList = useMemo(() => {
+//     const keyword = debounced.replace(/[a-zA-Z\uAC00-\uD7AF]/g, (match) => match.toLowerCase())
+//     console.log(keyword)
+//     return songList.filter((song) => song.title.includes(keyword))
+//   }, [songList, debounced])
+//
+//   const selectedSongIds = useMemo(() => selectedSongInfoList.map((info) => info?.song?.id), [selectedSongInfoList])
+//
+//   return (
+//     <div
+//       className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-4 gap-y-10 mt-10">
+//       {
+//         preprocessedSongList.map((song: Song) => (
+//           <SongSelectCard
+//             teamId={teamId}
+//             key={song.id}
+//             songId={song.id}
+//             isSelected={selectedSongIds.includes(song.id)}
+//           />
+//         ))
+//       }
+//     </div>
+//   )
+// }
--- app/board/_components/bottom-navbar/create-worship-button.tsx
+++ app/board/_components/bottom-navbar/create-worship-button.tsx
@@ -1,30 +1,23 @@
-import {useState} from "react";
-import {useRecoilValue, useSetRecoilState} from "recoil";
-import {currentTeamIdAtom} from "@/global-states/teamState";
-import {selectedSongInfoListAtom} from "@/app/board/[teamId]/plan/_components/status";
-import {WorshipForm} from "@/app/board/[teamId]/plan/_components/worship-form";
-import {FormMode} from "@/components/constants/enums";
+import {useRecoilValue} from "recoil";
 import {LayoutDashboard} from "lucide-react";
+import {useRouter} from "next/navigation";
+import {getPathCreatePlan} from "@/components/helper/routes";
+import {currentTeamIdAtom} from "@/global-states/teamState";
 
 export function CreateWorshipButton() {
-  const [isOpen, setIsOpen] = useState(false)
   const teamId = useRecoilValue(currentTeamIdAtom)
-  const setSelectedSongInfoList = useSetRecoilState(selectedSongInfoListAtom)
+  const router = useRouter()
 
   function handleClick() {
-    setSelectedSongInfoList([])
-    setIsOpen(true)
+    router.push(getPathCreatePlan(teamId))
   }
 
   return (
-    <>
-      <WorshipForm mode={FormMode.CREATE} isOpen={isOpen} setIsOpen={setIsOpen} worship={null}/>
-      <div className="flex-center flex-col space-y-1 cursor-pointer" onClick={handleClick}>
-        <div className="flex-center w-20 h-20 bg-gray-300 rounded-lg">
-          <LayoutDashboard/>
-        </div>
-        <p className="text-sm">Worship</p>
+    <div className="flex-center flex-col space-y-1 cursor-pointer" onClick={handleClick}>
+      <div className="flex-center w-20 h-20 bg-gray-300 rounded-lg">
+        <LayoutDashboard/>
       </div>
-    </>
+      <p className="text-sm">Worship</p>
+    </div>
   )
 }
