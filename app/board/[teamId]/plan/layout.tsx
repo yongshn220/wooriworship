a0a8e925e3c7f41850284324a293df56210e41c5
Yongjung Shin <yongshn220@gmail.com>
2024-06-30 23:49:43 -0400

    fix build

--- app/board/[teamId]/plan/layout.tsx
+++ app/board/[teamId]/plan/layout.tsx
@@ -2,7 +2,7 @@
 
 import {PullToRefresh} from "@/components/functionality/pull-to-refresh";
 
-export default function PlanLayout({children}) {
+export default function PlanLayout({children}: any) {
   return (
     <PullToRefresh>
       {children}
