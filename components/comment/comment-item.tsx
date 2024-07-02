3db5ebd7180518ca9848d0d3644e50329852c685
Yongjung Shin <yongshn220@gmail.com>
2024-07-01 20:46:16 -0400

    update | song place holder

--- components/comment/comment-item.tsx
+++ components/comment/comment-item.tsx
@@ -1,24 +1,46 @@
 import {Badge} from "@/components/ui/badge";
-import {UserIcon} from "lucide-react";
+import {EllipsisIcon, UserIcon} from "lucide-react";
+import {CommentItemMenu} from "@/components/comment/comment-item-menu";
+import React, {useState} from "react";
+import {Textarea} from "@/components/ui/textarea";
 
 
 export function CommentItem() {
+  const [isEditMode, setIsEditMode] = useState(false)
+
+  const email = "yongjung.shin@gmail.com"
+
   return (
     <div className="flex flex-col py-5 gap-4 border-b border-gray-300">
       <div className="w-full flex justify-between">
-        <div className="w-full flex-between flex-col gap-2 sm:flex-row sm:gap-5">
-          <div className="flex flex-center gap-2">
+        <div className="w-full flex-between gap-2 sm:gap-5">
+          <div className="w-full flex gap-2">
             <UserIcon/>
             <p className="text-sm font-semibold">
-              yongjung.shin@gmail.com
+              {email.includes('@') ? email.split('@')[0] : email}
             </p>
-            {true && <Badge className="bg-blue-400 hover:bg-blue-400">Me</Badge>}
+            {true && <Badge className="text-xs bg-blue-400 hover:bg-blue-400">Me</Badge>}
+            <CommentItemMenu setIsEditMode={setIsEditMode}/>
           </div>
-          <p className="text-sm text-gray-500">1 months ago</p>
+          <p className="w-full text-sm text-gray-500 text-right">1 min</p>
         </div>
       </div>
-        <p className="text-sm leading-6 text-gray-900">This is a test comment. Comment will be available soon.</p>
+        {
+          isEditMode ?
+            <div className="w-full flex-end flex-col">
+              <Textarea className="h-20" placeholder="Create a comment" disabled={true}/>
+              <div className="w-full flex-end mt-2 gap-4">
+                <p className="text-sm text-gray-500 hover:text-gray-700 cursor-pointer"
+                   onClick={() => setIsEditMode(false)}>Cancel</p>
+                <p className="text-sm text-blue-500 hover:text-blue-700 cursor-pointer"
+                   onClick={() => setIsEditMode(false)}>Save</p>
+              </div>
+            </div>
+            :
+            <p className="text-sm leading-6 text-gray-900">This is a test comment. Comment will be available soon.</p>
+        }
       <div>
+
       </div>
     </div>
   )
