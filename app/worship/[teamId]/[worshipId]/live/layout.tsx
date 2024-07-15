f2fb3b0c91dad30c0e2f05ef0e8c5c22335e0dd9
Yongjung Shin <yongshn220@gmail.com>
2024-07-15 16:06:12 -0400

    Cleaning

--- app/worship/[teamId]/[worshipId]/live/layout.tsx
+++ app/worship/[teamId]/[worshipId]/live/layout.tsx
@@ -3,7 +3,7 @@
 import {WorshipLivePreference} from "@/app/worship/[teamId]/[worshipId]/live/_components/worship-live-preference";
 
 
-export default function WorshipLiveLayout({children}) {
+export default function WorshipLiveLayout({children}: any) {
   return (
     <div className="w-full h-full">
       <WorshipLivePreference/>
