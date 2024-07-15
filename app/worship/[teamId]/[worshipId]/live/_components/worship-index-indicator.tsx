f1a1e66ab9b4e61e7a36fa96515944e5b5c59ab8
Yongjung Shin <yongshn220@gmail.com>
2024-07-15 16:19:05 -0400

    update | worship live number indicater size

--- app/worship/[teamId]/[worshipId]/live/_components/worship-index-indicator.tsx
+++ app/worship/[teamId]/[worshipId]/live/_components/worship-index-indicator.tsx
@@ -14,12 +14,15 @@ export function WorshipIndexIndicator() {
   const setWorshipIndexChangeEvent = useSetRecoilState(worshipIndexChangeEventAtom)
 
   return (
-    <div className={cn("absolute left-5 gap-4", {"flex-center flex-col": menu.showSongNumber}, {"hidden": !menu.showSongNumber})}>
+    <div className={cn("absolute left-5 gap-y-2", {"flex-center flex-col": menu.showSongNumber}, {"hidden": !menu.showSongNumber})}>
       {
         Array.from(Array(index.total)).map((_, i) => (
-          (i !== index.current)
-            ? <div key={i} className="w-3 h-3 bg-gray-300 rounded-full cursor-pointer hover:bg-blue-300" onClick={() => setWorshipIndexChangeEvent(i)}/>
-            : <div key={i} className="flex-center w-6 h-6 bg-blue-500 rounded-lg font-semibold text-white">{i + 1}</div>
+          (i !== index.current) ?
+            <div key={i} className="group w-6 h-6 flex-center cursor-pointer" onClick={() => setWorshipIndexChangeEvent(i)}>
+              <div className="w-3 h-3 bg-gray-300 rounded-full group-hover:bg-blue-300"/>
+            </div>
+            :
+            <div key={i} className="flex-center w-6 h-6 bg-blue-500 rounded-lg font-semibold text-white">{i + 1}</div>
         ))
       }
     </div>
