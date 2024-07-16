f81e7b264ad1e127001e2fa7a00d871bf79b0a86
Yongjung Shin <yongshn220@gmail.com>
2024-07-16 19:50:37 -0400

    Update | worship card UI

--- app/board/[teamId]/plan/_components/worship-card.tsx
+++ app/board/[teamId]/plan/_components/worship-card.tsx
@@ -2,12 +2,18 @@
 
 import {HoverOverlay} from "@/components/hover-overlay";
 import Link from "next/link";
-import {getDayByTimestamp, isTimestampPast, timestampToDateString} from "@/components/helper/helper-functions";
+import {
+  getDayByTimestamp,
+  isTimestampPast,
+  timestampToDateString,
+  timestampToDateStringFormatted
+} from "@/components/helper/helper-functions";
 import {getPathWorship} from "@/components/helper/routes";
 import {useRecoilValue, useRecoilValueLoadable} from "recoil";
 import {currentTeamIdAtom} from "@/global-states/teamState";
 import {worshipAtom, worshipSongListAtom} from "@/global-states/worship-state";
 import {cn} from "@/lib/utils";
+import {useRouter} from "next/navigation";
 
 interface Props {
   worshipId: string
@@ -17,30 +23,56 @@ export function WorshipCard({worshipId}: Props) {
   const teamId = useRecoilValue(currentTeamIdAtom)
   const worship = useRecoilValue(worshipAtom(worshipId))
   const worshipSongListLoadable = useRecoilValueLoadable(worshipSongListAtom(worshipId))
+  const router = useRouter()
 
   return (
-    <div>
-      <Link href={getPathWorship(teamId, worship.id)}>
-        <div className={cn("group md:aspect-[1/1] border rounded-lg flex flex-col overflow-hidden cursor-pointer", {"bg-gray-400": isTimestampPast(worship?.worship_date)}, {"bg-[#95ABCC]" : !isTimestampPast(worship?.worship_date)})}>
-          <div className="relative flex-1 flex-center flex-col text-white text-xs font-semibold gap-2 p-2 min-h-[150px]">
-            <HoverOverlay/>
-            { worshipSongListLoadable.state === 'loading' && <></> }
-            { worshipSongListLoadable.state === 'hasError' && <></> }
-            {
-              worshipSongListLoadable.state === 'hasValue' &&
-              worshipSongListLoadable.contents.map((song, i) => (
-                <p key={i} className="line-clamp-1 text-base sm:text-sm">{song?.title}</p>
+    <div onClick={() => router.push(getPathWorship(teamId, worship.id))}>
+      <div className="group md:aspect-[1/1] rounded-lg flex flex-col bg-white overflow-hidden cursor-pointer shadow p-4">
+        <div className="flex-start flex-col pb-2 border-b">
+          <p className="w-full text-sm text-gray-600 mt-1">
+            {timestampToDateStringFormatted(worship?.worship_date)} <span
+            className="text-xs">({getDayByTimestamp(worship?.worship_date)})</span>
+          </p>
+          <div className="font-semibold text-lg">{worship?.title}</div>
+        </div>
+        <div className="h-full flex flex-col justify-start gap-3 py-4">
+          {
+            worshipSongListLoadable.state === 'hasValue' &&
+            worshipSongListLoadable.contents.map((song, i) => (
+                <div key={i} className="flex items-center gap-2">
+                  <div className="w-1.5 h-1.5 rounded-full bg-blue-500"></div>
+                  <p className="line-clamp-1 text-base sm:text-sm">{song?.title}</p>
+                </div>
               ))
             }
-          </div>
-          <p className="p-4 bg-white line-clamp-1">
-            {worship?.title !== ""? worship.title : "No title" }
-          </p>
         </div>
-      </Link>
-      <p className="w-full text-center text-sm text-gray-600 mt-1">
-        {timestampToDateString(worship?.worship_date)} <span className="text-xs">({getDayByTimestamp(worship?.worship_date)})</span>
-      </p>
+      </div>
     </div>
   )
+
+  // return (
+  //   <div>
+  //     <Link href={getPathWorship(teamId, worship.id)}>
+  //       <div className={cn("group md:aspect-[1/1] border rounded-lg flex flex-col overflow-hidden cursor-pointer", {"bg-gray-400": isTimestampPast(worship?.worship_date)}, {"bg-[#95ABCC]" : !isTimestampPast(worship?.worship_date)})}>
+  //         <div className="relative flex-1 flex-center flex-col text-white text-xs font-semibold gap-2 p-2 min-h-[150px]">
+  //           <HoverOverlay/>
+  //           { worshipSongListLoadable.state === 'loading' && <></> }
+  //           { worshipSongListLoadable.state === 'hasError' && <></> }
+  //           {
+  //             worshipSongListLoadable.state === 'hasValue' &&
+  //             worshipSongListLoadable.contents.map((song, i) => (
+  //               <p key={i} className="line-clamp-1 text-base sm:text-sm">{song?.title}</p>
+  //             ))
+  //           }
+  //         </div>
+  //         <p className="p-4 bg-white line-clamp-1">
+  //           {worship?.title !== ""? worship.title : "No title" }
+  //         </p>
+  //       </div>
+  //     </Link>
+  //     <p className="w-full text-center text-sm text-gray-600 mt-1">
+  //       {timestampToDateString(worship?.worship_date)} <span className="text-xs">({getDayByTimestamp(worship?.worship_date)})</span>
+  //     </p>
+  //   </div>
+  // )
 }
