524ec39f20b9cc11ac8976ab452b4cd976f46c4b
Yongjung Shin <yongshn220@gmaill.com>
2024-07-18 15:53:13 -0400

    Update | edit song comment

--- components/comment/comment-item.tsx
+++ components/comment/comment-item.tsx
@@ -3,11 +3,13 @@ import {UserIcon} from "lucide-react";
 import {CommentItemMenu} from "@/components/comment/comment-item-menu";
 import React, {useState} from "react";
 import {Textarea} from "@/components/ui/textarea";
-import {useRecoilValue} from "recoil";
-import {songCommentAtom} from "@/global-states/song-comment-state";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {songCommentAtom, songCommentUpdater} from "@/global-states/song-comment-state";
 import {userAtom} from "@/global-states/userState";
 import {auth} from "@/firebase";
 import {timestampToDatePassedFromNow, timestampToDatePassedFromNowShorten} from "@/components/helper/helper-functions";
+import {SongCommentService} from "@/apis";
+import {toast} from "@/components/ui/use-toast";
 
 
 interface Props {
@@ -15,11 +17,22 @@ interface Props {
 }
 
 export function CommentItem({commentId}: Props) {
+  const setCommentUpdater = useSetRecoilState(songCommentUpdater)
   const comment = useRecoilValue(songCommentAtom(commentId))
   const user = useRecoilValue(userAtom(comment?.created_by.id))
   const currentUser = auth.currentUser
   const [isEditMode, setIsEditMode] = useState(false)
+  const [commentInput, setCommentInput] = useState<string>(comment?.comment ?? "")
 
+  async function handleEditComment() {
+    if (!await SongCommentService.updateSongComment(commentId, commentInput)) {
+      toast({description: "Fail to edit comment. Please try again."})
+      return;
+    }
+    setCommentUpdater(prev => prev + 1)
+    setIsEditMode(false)
+    toast({description: "Comment updated successfully."})
+  }
 
   return (
     <div className="flex flex-col py-5 gap-4 border-b border-gray-300">
@@ -37,13 +50,13 @@ export function CommentItem({commentId}: Props) {
       </div>
         {
           isEditMode ?
-            <div className="w-full flex-end flex-col">
-              <Textarea className="h-20" placeholder="Create a comment" disabled={true}/>
+            <div className="w-full flex-end flex-col p-2">
+              <Textarea value={commentInput} onChange={(e) => setCommentInput(e.target.value)} className="h-20" />
               <div className="w-full flex-end mt-2 gap-4">
                 <p className="text-sm text-gray-500 hover:text-gray-700 cursor-pointer"
                    onClick={() => setIsEditMode(false)}>Cancel</p>
                 <p className="text-sm text-blue-500 hover:text-blue-700 cursor-pointer"
-                   onClick={() => setIsEditMode(false)}>Save</p>
+                   onClick={() => handleEditComment()}>Save</p>
               </div>
             </div>
             :
