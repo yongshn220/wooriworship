476d5cdf87c82fd88203217938920238d897a883
Yongjung Shin <yongshn220@gmail.com>
2024-08-13 20:38:41 -0400

    Update | song key box

--- apis/BaseService.ts
+++ apis/BaseService.ts
@@ -1,157 +1,187 @@
 import {firestore} from "@/firebase";
 
 export default class BaseService {
-    collectionName: string;
-    constructor(collectionName: string) {
-        this.collectionName = collectionName;
-    }
+  collectionName: string;
 
-    async getById(id: any) {
-        try {
-            const ref = firestore.collection(this.collectionName).doc(id);
-            const res = await ref.get();
-            if(res.exists) {
-                return {id: id, ... res.data()};
-            } else {
-                return null;
-            }
-        }
-        catch (e) {
-            console.log(e)
-            return null
-        }
-    }
+  constructor(collectionName: string) {
+    this.collectionName = collectionName;
+  }
 
-    async getByIds(ids: Array<string>|undefined) {
-        if(ids == null) {
-            return [];
-        }
-        return await this.queryByArray({
-            a: '__name__',
-            b: 'in',
-            c: ids
-        });
+  async getById(id: any) {
+    try {
+      const ref = firestore.collection(this.collectionName).doc(id);
+      const res = await ref.get();
+      if (res.exists) {
+        return {id: id, ...res.data()};
+      } else {
+        return null;
+      }
+    } catch (e) {
+      console.log(e)
+      return null
     }
+  }
 
-    async getByEmail(email: string) {
-        const query = await firestore.collection(this.collectionName).where('email', '==', email).get();
-        if (!query.empty) {
-            // Assuming there's only one user per email, so we're getting the first document
-            const doc = query.docs[0];
-            return { id: doc.id, ...doc.data() };
-        }
-        else {
-            console.log("err: getByEamil")
-            return null;
-        }
+  async getByIds(ids: Array<string> | undefined) {
+    if (ids == null) {
+      return [];
     }
+    return await this.queryByArray({
+      a: '__name__',
+      b: 'in',
+      c: ids
+    });
+  }
 
-    async getByFilters(filters: Array<any> | null) {
-        try {
-            const result: Array<any> = [];
-            let ref: any = firestore.collection(this.collectionName);
-            if(filters) {
-                for(let i in filters) {
-                    ref = ref.where(filters[i].a, filters[i].b, filters[i].c);
-                }
-                ref = await ref.get();
-            } else {
-                ref = await ref.get();
-            }
-            ref.forEach((element: any) => {
-                result.push({id: element.id, ... element.data()});
+  async getByEmail(email: string) {
+    const query = await firestore.collection(this.collectionName).where('email', '==', email).get();
+    if (!query.empty) {
+      // Assuming there's only one user per email, so we're getting the first document
+      const doc = query.docs[0];
+      return {id: doc.id, ...doc.data()};
+    } else {
+      console.log("err: getByEamil")
+      return null;
+    }
+  }
+
+  async getByFilters(filters: Array<any> | null) {
+    try {
+      const result: Array<any> = [];
+      let ref: any = firestore.collection(this.collectionName);
+      if (filters) {
+        for (let i in filters) {
+          ref = ref.where(filters[i].a, filters[i].b, filters[i].c);
+        }
+        ref = await ref.get();
+      } else {
+        ref = await ref.get();
+      }
+      ref.forEach((element: any) => {
+        result.push({id: element.id, ...element.data()});
+      })
+      return result;
+    } catch (e) {
+      console.log(e)
+      return null
+    }
+  }
+
+  async getByFiltersWithPagination(filters: Array<any> | null, lastDoc: any = null, pageSize: number = 10) {
+    try {
+      const result: Array<any> = [];
+      let cRef: any = firestore.collection(this.collectionName);
+
+      if (filters) {
+        for (let i in filters) {
+          cRef = cRef.where(filters[i].a, filters[i].b, filters[i].c);
+        }
+      }
+
+      // 정렬 기준 추가 (예: 생성 시간순)
+      cRef = cRef.orderBy('createdAt', 'desc');
+
+      // 페이지네이션 적용
+      if (lastDoc) {
+        cRef = cRef.startAfter(lastDoc);
+      }
+      cRef = cRef.limit(pageSize);
+
+      const snapshot = await cRef.get();
+
+      snapshot.forEach((doc: any) => {
+        result.push({id: doc.id, ...doc.data()});
+      });
+
+      // 마지막 문서 반환
+      const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+
+      return {songs: result, lastDoc: lastVisible};
+    }
+    catch (e) {
+      console.log(e);
+      return null;
+    }
+  }
+
+  async deleteByFilters(filters: Array<any> | null) {
+    try {
+      const refs = await this.getByFilters(filters);
+      const promises = [];
+      for (const ref of refs) {
+        promises.push(this.delete(ref.id))
+      }
+      await Promise.all(promises);
+      return true;
+    } catch (e) {
+      console.log(e)
+      return false;
+    }
+  }
+
+  async queryByArray(filter: any) {
+    try {
+      if (filter) {
+        const promises = [];
+        const result: any = [];
+        console.log(filter.c);
+        while (filter.c.length) {
+          const subFilters = filter.c.splice(0, 10);
+          promises.push(
+            firestore.collection(this.collectionName).where(
+              filter.a,
+              filter.b,
+              subFilters
+            ).get().then(x => {
+              x.forEach(element => {
+                result.push({id: element.id, ...element.data()});
+              })
             })
-            return result;
+          )
         }
-        catch (e) {
-            console.log(e)
-            return null
-        }
+        await Promise.all(promises);
+        return result;
+      }
+      console.log("queryByArray: filter is not exist.")
+      return null;
+    } catch (e) {
+      console.log(e)
+      return null
     }
+  }
 
-    async deleteByFilters(filters: Array<any> | null) {
-        try {
-            const refs = await this.getByFilters(filters);
-            const promises = [];
-            for(const ref of refs) {
-                promises.push(this.delete(ref.id))
-            }
-            await Promise.all(promises);
-            return true;
-        }
-        catch (e) {
-            console.log(e)
-            return false;
-        }
-    }
+  async getAll() {
+    return await this.getByFilters(null);
+  }
 
-    async queryByArray(filter: any) {
-        try {
-            if(filter) {
-                const promises = [];
-                const result:any = [];
-                console.log(filter.c);
-                while (filter.c.length) {
-                    const subFilters = filter.c.splice(0, 10);
-                    promises.push(
-                        firestore.collection(this.collectionName).where(
-                            filter.a,
-                            filter.b,
-                            subFilters
-                        ).get().then(x => {
-                            x.forEach(element => {
-                                result.push({id: element.id, ... element.data()});
-                            })
-                        })
-                    )
-                }
-                await Promise.all(promises);
-                return result;
-            }
-            console.log("queryByArray: filter is not exist.")
-            return null;
-        }
-        catch (e) {
-            console.log(e)
-            return null
-        }
+  async create(data: any) {
+    try {
+      const ref = await firestore.collection(this.collectionName).add(data);
+      return ref.id;
+    } catch (e) {
+      console.log(e)
+      return null
     }
+  }
 
-    async getAll() {
-        return await this.getByFilters(null);
+  async update(id: string, data: any) {
+    try {
+      await firestore.collection(this.collectionName).doc(id).set(data, {merge: true});
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
     }
+  }
 
-    async create(data: any) {
-        try {
-            const ref = await firestore.collection(this.collectionName).add(data);
-            return ref.id;
-        }
-        catch (e) {
-            console.log(e)
-            return null
-        }
+  async delete(id: string) {
+    try {
+      await firestore.collection(this.collectionName).doc(id).delete();
+      return true
+    } catch (e) {
+      console.log(e)
+      return false
     }
-
-    async update(id: string, data: any) {
-        try {
-            await firestore.collection(this.collectionName).doc(id).set(data, {merge: true});
-            return true
-        }
-        catch (e) {
-            console.log(e)
-            return false
-        }
-    }
-
-    async delete(id: string) {
-        try {
-            await firestore.collection(this.collectionName).doc(id).delete();
-            return true
-        }
-        catch (e) {
-            console.log(e)
-            return false
-        }
-    }
+  }
 }
 
--- components/song/song-key-box.tsx
+++ components/song/song-key-box.tsx
@@ -5,6 +5,11 @@ export function SongKeyBox({musicSheetId}: {musicSheetId: string}) {
   const musicSheet = useRecoilValue(musicSheetAtom(musicSheetId))
 
   return (
-    <div className="flex-center text-xs text-white font-medium bg-gray-400 rounded-sm w-5 h-5">{musicSheet?.key}</div>
+    <>
+      {
+        musicSheet?.key &&
+        <div className="flex-center text-xs text-white font-medium bg-gray-400 rounded-sm min-w-5 min-h-5 p-1">{musicSheet?.key}</div>
+      }
+    </>
   )
 }
