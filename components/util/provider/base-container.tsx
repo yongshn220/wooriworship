ca3d6aba6e77b2287b13fcae825943dcc15fe38c
Yongjung Shin <yongshn220@gmail.com>
2025-02-01 19:41:01 -0500

    Update | max width to full width

--- components/util/provider/base-container.tsx
+++ components/util/provider/base-container.tsx
@@ -1,7 +1,7 @@
 export function BaseContainer({children}: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex-center w-full h-full">
-      <div className="w-full h-full max-w-5xl border-l border-r">
+      <div className="w-full h-full border-l border-r">
         {children}
       </div>
     </div>
