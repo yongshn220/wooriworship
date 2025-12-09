efe314d01833d77e0d8600055281f65fe3e92ddb
User <user@example.com>
2025-12-09 15:08:14 -0500

    Update"

--- app/board/[teamId]/(worship)/worship-board/_components/worship-card-list.tsx
+++ app/board/[teamId]/(worship)/worship-board/_components/worship-card-list.tsx
@@ -1,16 +1,16 @@
-import {useRecoilValueLoadable} from "recoil";
+import { useRecoilValueLoadable } from "recoil";
 import * as React from "react";
-import {currentTeamWorshipIdsAtom} from "@/global-states/worship-state";
-import {Suspense} from "react";
-import {WorshipCard} from "@/app/board/[teamId]/(worship)/worship-board/_components/worship-card";
-import {EmptyWorshipBoardPage} from "@/app/board/[teamId]/(worship)/worship-board/_components/empty-worship-board-page/empty-worship-board-page";
+import { currentTeamWorshipIdsAtom } from "@/global-states/worship-state";
+import { Suspense } from "react";
+import { WorshipCard } from "@/app/board/[teamId]/(worship)/worship-board/_components/worship-card";
+import { EmptyWorshipBoardPage } from "@/app/board/[teamId]/(worship)/worship-board/_components/empty-worship-board-page/empty-worship-board-page";
 
 
 interface Props {
   teamId: string
 }
 
-export function WorshipCardList({teamId}: Props) {
+export function WorshipCardList({ teamId }: Props) {
   const worshipIdsLoadable = useRecoilValueLoadable(currentTeamWorshipIdsAtom(teamId))
 
   switch (worshipIdsLoadable.state) {
@@ -21,17 +21,17 @@ export function WorshipCardList({teamId}: Props) {
         <div className="w-full h-full">
           {
             (worshipIdsLoadable.contents?.length > 0) ?
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-10 grid-flow-row-dense grid-rows-[auto]">
-              {
-                worshipIdsLoadable.contents.map((worshipId: string) => (
-                  <Suspense key={worshipId} fallback={<div></div>}>
-                    <WorshipCard worshipId={worshipId}/>
-                  </Suspense>
-                ))
-              }
-            </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-10 grid-flow-row-dense grid-rows-[auto]">
+                {
+                  worshipIdsLoadable.contents.map((worshipId: string, index: number) => (
+                    <Suspense key={worshipId} fallback={<div></div>}>
+                      <WorshipCard worshipId={worshipId} isFirst={index === 0} />
+                    </Suspense>
+                  ))
+                }
+              </div>
               :
-            <EmptyWorshipBoardPage/>
+              <EmptyWorshipBoardPage />
           }
         </div>
       )
