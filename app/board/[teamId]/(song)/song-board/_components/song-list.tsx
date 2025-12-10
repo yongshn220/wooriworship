ee9f96836d9e61ff8deea90c156c13c5d09d28a3
User <user@example.com>
2025-12-10 13:59:15 -0500

    update

--- app/board/[teamId]/(song)/song-board/_components/song-list.tsx
+++ app/board/[teamId]/(song)/song-board/_components/song-list.tsx
@@ -49,7 +49,7 @@ export function SongList({ teamId }: Props) {
       {/* Header Row for Desktop */}
       <div className="hidden md:flex items-center px-6 py-2 mb-2 text-xs font-semibold text-gray-500 uppercase tracking-wider">
         <div className="flex-1 pl-4">Title</div>
-        <div className="w-20 shrink-0 text-center">Key</div>
+        <div className="w-32 shrink-0 text-center">Key</div>
       </div>
 
       <div className="flex flex-col space-y-2">
