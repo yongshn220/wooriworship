ac6c24ea7caa8b3aff5b07837582cb70e5632d5a
User <user@example.com>
2025-12-10 15:56:24 -0500

    update

--- app/board/[teamId]/(song)/song-board/_components/song-card.tsx
+++ app/board/[teamId]/(song)/song-board/_components/song-card.tsx
@@ -22,8 +22,8 @@ export function SongCard({ teamId, songId, index }: Props) {
 
     return (
         <motion.div
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{
                 type: "spring",
                 stiffness: 260,
