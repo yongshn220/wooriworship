6309e00ea96c0c17c76760dc5798e4f960853ba0
User <user@example.com>
2025-12-11 11:12:46 -0500

    update

--- app/board/_components/board-navigation/board-top-nav-bar/board-top-nav-bar.tsx
+++ app/board/_components/board-navigation/board-top-nav-bar/board-top-nav-bar.tsx
@@ -134,7 +134,7 @@ export function BoardTopNavBar() {
               className="w-full flex items-center justify-between"
             >
               {/* Left Side: Title or Logo */}
-              <div className="flex items-center gap-3">
+              <div className="flex-center flex-col w-full h-full">
                 {currentConfig.showLogo ? (
                   <div className="opacity-90 hover:opacity-100 transition-opacity">
                     <MainLogoSmall />
