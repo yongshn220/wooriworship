a38dea76676f33445b345c70b80414909383997b
User <user@example.com>
2025-12-11 16:20:09 -0500

    update

--- app/board/[teamId]/(worship)/worship-view/[worshipId]/_components/worship-live-carousel-item.tsx
+++ app/board/[teamId]/(worship)/worship-view/[worshipId]/_components/worship-live-carousel-item.tsx
@@ -65,11 +65,11 @@ export function WorshipLiveCarouselItem({ index, urls }: WorshipLiveCarouselItem
             <div className="h-full w-full flex flex-col bg-white dark:bg-black overflow-y-auto scrollbar-hide">
                 {
                     urls.map((url, index) => (
-                        <div key={index} className="flex-center w-full h-full p-1">
+                        <div key={index} className="flex-center w-full h-full p-1 select-none" style={{ WebkitTouchCallout: "none" }} onContextMenu={(e) => e.preventDefault()}>
                             <img
                                 alt="Music score"
                                 src={url}
-                                className="max-w-full max-h-full object-contain shadow-sm"
+                                className="max-w-full max-h-full object-contain shadow-sm select-none pointer-events-none"
                             />
                         </div>
                     ))
