af0c348aad0c170a5d1647c310b6d7037daacde7
User <user@example.com>
2025-12-12 17:23:35 -0500

    update

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -14,7 +14,7 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProvider swipeDirection="right">
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
